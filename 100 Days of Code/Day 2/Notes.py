bda22d4a1e9dfe1a7afa16b584363612dee3ed21
terrahung <terra_hong@outlook.com>
2024-12-29 22:45:58 +0800

    Day 2 notes

---
+++ 100 Days of Code/Day 2/Notes.py
@@ -0,0 +1,15 @@
+score = 0
+
+# user scores a point
+score += 1
+print(score)
+
+# f-strings
+print("Your score is " + str(score))
+
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"Your score is {score}, your height is {height}, you are winning is {is_winning}")
+
