a14f67a9b8e6c59c1b008758935ccb5a435d99da
terrahung <terra_hong@outlook.com>
2024-12-29 22:45:04 +0800

    Updated filename

---
+++ 100 Days of Code/Day 2/Subscripting.py
@@ -0,0 +1,19 @@
+# Subscripting
+print("Hello"[0])
+
+# String concatenation
+print("123" + "456") # concatenation
+
+# Integer = Whole number
+print(123 + 456)
+
+# Large Integers
+print(123456789)
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
+
